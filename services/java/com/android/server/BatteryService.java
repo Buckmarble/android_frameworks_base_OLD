a4c74325d3bad96df78b4d44f72a1b51020c885c
buckmarble <forrestmarble@gmail.com>
2012-10-14 12:33:57 -0400

    hopefully fixing again

--- services/java/com/android/server/BatteryService.java
+++ services/java/com/android/server/BatteryService.java
@@ -164,7 +164,7 @@ class BatteryService extends Binder {
                 com.android.internal.R.integer.config_lowBatteryCloseWarningLevel);
 
         mHasDockBattery = mContext.getResources().getBoolean(
-                com.android.internal.R.bool.config_hasDockBattery);
+                com.android.internal.R.bool.config_mHasDockBattery);
 
         mPowerSupplyObserver.startObserving("SUBSYSTEM=power_supply");
 
