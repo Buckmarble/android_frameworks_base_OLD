7e4393052978d21d8c9094bf3ddd09750cb3be94
Chirayu Desai <cdesai@cyanogenmod.org>
2013-03-29 11:56:43 +0530

    SettingsProvider: Initialize defaults for CM features
    Add configuration defaults for:
    - status bar notification count
    - QuickSettings quick pulldown
    - lockscreen targets
    
    Change-Id: Ief4b97fcceea7e351a13d59136a2fb1823d97b04
    
    Conflicts:
    
    	packages/SettingsProvider/res/values/defaults.xml
    	packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -1969,6 +1969,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
             loadIntegerSetting(stmt, Settings.System.POINTER_SPEED,
                     R.integer.def_pointer_speed);
+
+            loadIntegerSetting(stmt, Settings.System.STATUS_BAR_BATTERY,
+                    R.integer.def_battery_style);
+
+            loadIntegerSetting(stmt, Settings.System.STATUS_BAR_NOTIF_COUNT,
+                    R.integer.def_notif_count);
+
+            loadIntegerSetting(stmt, Settings.System.QS_QUICK_PULLDOWN,
+                    R.integer.def_qs_quick_pulldown);
+
+            loadStringSetting(stmt, Settings.System.LOCKSCREEN_TARGETS,
+                    R.string.def_lockscreen_targets);
         } finally {
             if (stmt != null) stmt.close();
         }
